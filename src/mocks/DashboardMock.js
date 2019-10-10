c65a33cd63ca33c248cbb59105e12e6048980763
Sergio Mata <sergio.mata@ivoy.mx>
2019-10-10 00:05:40 -0500

    feat: add mock information

---
+++ src/mocks/DashboardMock.js
@@ -0,0 +1,38 @@
+export const dashboardInfo = {
+    "revenue": {
+        "gain": 200000,
+        "total": 1000000,
+        "tablet": {
+            "porcent": 60,
+            "gain": 120000
+        },
+        "smartphone": {
+            "porcent": 40,
+            "gain": 80000
+        }
+    },
+    "impresions": {
+        "gain": 50000000,
+        "total": 90000000,
+        "tablet": {
+            "porcent": 40,
+            "gain": 20000000
+        },
+        "smartphone": {
+            "porcent": 60,
+            "gain": 30000000
+        }
+    },
+    "visits": {
+        "gain": 600000000,
+        "total": 1000000000,
+        "tablet": {
+            "porcent": 80,
+            "gain": 480000000
+        },
+        "smartphone": {
+            "porcent": 20,
+            "gain": 120000000
+        }
+    }
+};
